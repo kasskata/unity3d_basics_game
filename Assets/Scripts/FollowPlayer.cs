3b44334dbd1a42410256a46e7ac0b2671defcb6c
kasskata <>
2022-03-14 20:45:29 +0200

    Homework Solved 02. Using GameObject

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -2,8 +2,9 @@
 
 public class FollowPlayer : MonoBehaviour
 {
-    public GameObject plane;
+    public Transform target;
     public Vector3 offset;
+    public float smoothSpeed = 0.125f;
 
     // Start is called before the first frame update
     void Start()
@@ -12,8 +13,10 @@ public class FollowPlayer : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = plane.transform.position + offset;
+        var desiredPosition = target.TransformPoint(offset);
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.LookAt(target);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float rotationSpeed;
     public float verticalInput;
+    public float horizontalInput;
+    public float rollInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,13 +17,25 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        rollInput = 0;
+        transform.Translate(Vector3.forward * speed);
+        
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
+        // get the user's vertical input
+        horizontalInput = Input.GetAxis("Horizontal");
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rollInput = -1;
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            rollInput = 1;
+        }
 
-        // move the plane forward at a constant rate
-        transform.Translate(Vector3.forward * speed);
-
-        // tilt the plane up/down based on up/down arrow keys
+        // tilt the plane up/down/left/right based on arrow keys
         transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime * horizontalInput);
+        transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime * rollInput);
     }   
 }
